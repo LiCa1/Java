821c5244f3f76a61db9de67dcd8a17297e2743b2
LiCa1 <39522653+LiCa1@users.noreply.github.com>
2018-05-29 09:00:01 -0400

    contains main(), creates & updates personel

--- Team/Team.java
+++ Team/Team.java
@@ -3,8 +3,8 @@
 //                 There 6 Java programs(classes) that work together to set up an 
 //				   organization of players for a professional baseball francise.
 //                 Team.java         - contains main(), creates & updates personel, 
-//                 Personnel.java,   - Sets up objects and populates players data,
-//  			   Player.java,      - provides variables and methods for types
+//                 Personnel.java    - Sets up objects and populates players data,
+//  			   Player.java       - provides variables and methods for types
 //                                     of players which can be any of the 3 bellow.
 //                 Pitcher.java      - inherits from the Player class,  
 //				   Regular.java      - inherits from the Player class,  
